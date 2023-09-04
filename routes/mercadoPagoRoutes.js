104af56796d591d75d38ffdeef8cd03fbb238604
Rogelio Trejo <rouge2504@gmail.com>
2023-09-03 22:32:50 -0600

    Add customer to server

--- routes/mercadoPagoRoutes.js
+++ routes/mercadoPagoRoutes.js
@@ -3,3 +3,14 @@ const passport = require('passport');
 module.exports = (app) =>{
     app.post('/api/payment/create', mercadoPagoController.createPayment)
 }
+module.exports = (app) =>{
+    app.post('/api/payment/createUser', mercadoPagoController.createClient)
+
+}
+
+module.exports = (app) =>{
+    app.post('/api/payment/findUser', mercadoPagoController.findClient)
+
+}
+
+
