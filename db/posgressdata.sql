b22221d0ddd0cdd516af6824cd8ecdab1a89120b
Rogelio Trejo <rouge2504@gmail.com>
2023-09-29 17:03:20 -0600

    Add my sql

---
+++ db/posgressdata.sql
@@ -0,0 +1,11 @@
+/*
+	Postgres cluster grupomexicobackend-db created
+  Username:    postgres
+  Password:    1LsPnulJvViD9hu
+  Hostname:    grupomexicobackend-db.internal
+  Flycast:     fdaa:3:2a4f:0:1::2
+  Proxy port:  5432
+  Postgres port:  5433
+  Connection string: postgres://postgres:1LsPnulJvViD9hu@grupomexicobackend-db.flycast:5432
+
+*/
